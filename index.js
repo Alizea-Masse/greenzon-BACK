66b915d4cd895b7d31e22c8415eb9cbced9da7a9
Alizéa Massé <alizea777@hotmail.fr>
2023-04-13 12:56:50 +0200

    setup connection to DB

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
+// Pour que la réponse ne soit pas undefind
+// limit pour que l'image puisse rentrer
+app.use(express.json({ limit: "10mb" }));
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res, next) => {
-    res.send('server is up and running')
-})
+//mongodb connection
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.error(err));
 
+
+
+//api
+app.get("/", (req, res, next) => {
+  res.send("server is up and running");
+});
+
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+});
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
+  console.log(`Server listening on port ${PORT}`);
 });
