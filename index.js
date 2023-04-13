3ef3991f4a9a11ed4e9b8dd3c1501d70d10da199
Alizéa Massé <alizea777@hotmail.fr>
2023-04-13 14:34:28 +0200

    send data from connectand signup form to the db and add them to bodyresponses

--- index.js
+++ index.js
@@ -20,29 +20,61 @@ mongoose
 
 //schema
 const userSchema = mongoose.Schema({
-    lastName: String,
-    firstName: String,
-    email: {
-      type: String,
-      unique: true,
-    },
-    password: String,
-    confirmPassword: String,
-    image: String,
-  });
+  lastName: String,
+  firstName: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  confirmPassword: String,
+  image: String,
+});
 
-  //model
+//model
 
-  const userModel = mongoose.model('user', userSchema);
+const userModel = mongoose.model("user", userSchema);
 
 //api
 app.get("/", (req, res, next) => {
   res.send("server is up and running");
 });
 
-app.post("/signup", (req, res) => {
-  console.log(req.body);
-});
+app.post("/signup", async (req, res) => {
+  const { email } = req.body;
+
+  const user = await userModel.findOne({
+    email: email,
+  });
+  //console.log(user)
+  if (user) {
+    res.send({ message: "Vous êtes déjà inscrit·e", alert: false });
+  } else {
+    const data = await userModel(req.body);
+    const save = data.save();
+    res.send({ message: "Inscrit·e avec succès !", alert: true });
+  }
+}),
+  //api login
+
+  app.post("/login", async (req, res) => {
+    const { email } = req.body;
+    const user = await userModel.findOne({ email: email });
+    if (user) {
+      const dataSend = {
+        id: user._id,
+        lastName: user.lastName,
+        firstName: user.firstName,
+        email: user.email,
+        image: user.image,
+      };
+      console.log(dataSend);
+      res.send({ message: "Vous êtes connecté·e !", alert: true, data: dataSend });
+    }else {
+      res.send({ message: "Ce compte n'existe pas", alert: false });
+    }
+  });
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
